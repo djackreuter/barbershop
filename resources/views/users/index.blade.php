8489bafd555474bf318efed77edb02fdb3f81f4b
Dylan Reuter <djreuter45@gmail.com>
2017-10-17 20:22:42 -0600

    added main html layout page

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
+@extends('layout')
 
-        <title>Laravel</title>
-    </head>
-    <body>
-        <ul>
-            @foreach ($users as $user)
-                <li>
-                    <a href="/users/{{ $user->id }}">
-                        {{ $user->userName }}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
-    </body>
-</html>
+@section('content')
+    <h1>Users</h1>
+    <ul>
+        @foreach ($users as $user)
+            <li>
+                <a href="/users/{{ $user->id }}">
+                    {{ $user->userName }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
