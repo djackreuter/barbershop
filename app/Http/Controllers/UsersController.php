1588a9beb941347e67ba818a1bac09001d5de8af
Dylan Reuter <djreuter45@gmail.com>
2017-10-20 19:16:42 -0600

    added content to barbershop.show template

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use App\Users;
 
 class UsersController extends Controller {
-    public function index() {
-		 $users = Users::all();
+	public function index() {
+		$users = Users::all();
 
-		 return view('users.index', compact('users'));
-	 }
+		return view('users.index', compact('users'));
+	}
 
-	 public function show(Users $user) {
+	public function show(Users $user) {
 
-		 return view('users.show', compact('user'));
-	 }
+		return view('users.show', compact('user'));
+	}
 }
---
+++ resources/views/barbershop/show.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Barbers</title>
+    </head>
+    <body>
+        <ul>
+            <li>Barbershop: {{ $barbershop->barbershopName }}</li>
+            <li>About Us: {{ $barbershop->barbershopBio }}</li>
+        </ul>
+    </body>
+</html>
