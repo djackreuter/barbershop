921d51abc0c3760210c8d624d420de084433db71
Jack Reuter <djreuter45@gmail.com>
2017-10-23 16:27:24 -0600

    cleaned up barbershop.show view by using directives

--- resources/views/barbershop/show.blade.php
+++ resources/views/barbershop/show.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html>
-    @include('layouts.head')
-    <body>
-        @include('layouts.nav')
-        <ul>
-            <li>Barbershop: {{ $barbershop->barbershopName }}</li>
-            <li>About Us: {{ $barbershop->barbershopBio }}</li>
-        </ul>
-        @include('layouts.footer')
-    </body>
-</html>
+@extends('layouts.master')
+
+@section('barbershop')
+<ul>
+    <li>Barbershop: {{ $barbershop->barbershopName }}</li>
+    <li>About Us: {{ $barbershop->barbershopBio }}</li>
+</ul>
+@endsection
