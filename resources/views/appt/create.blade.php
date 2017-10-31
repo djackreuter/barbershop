f4cd3e4cef4666445ed1810c5573b1947869d188
Jack Reuter <djreuter45@gmail.com>
2017-10-30 19:23:23 -0600

    added error handling to its own directive and referenced it in view

--- resources/views/appt/create.blade.php
+++ resources/views/appt/create.blade.php
@@ -58,16 +58,6 @@
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
 
-        @if(count($errors))
-            <div class="form-group">
-                <div class="alert alert-danger">
-                    <ul>
-                         @foreach($errors->all() as $error)
-                             <li>{{ $error }}</li>
-                         @endforeach
-                    </ul>
-                </div><!-- /.alert alert-danger -->
-            </div><!-- /.form-group -->
-        @endif
+        @include('layouts.errors')
     </form>
 @endsection
