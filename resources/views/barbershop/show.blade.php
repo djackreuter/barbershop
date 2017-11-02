a4299ba41b19e31c58b9158af6e32485b5eea380
Jack Reuter <djreuter45@gmail.com>
2017-11-01 19:00:29 -0600

    putting create appt form in barbershop view so that barbershop id is more accessable

--- resources/views/barbershop/show.blade.php
+++ resources/views/barbershop/show.blade.php
@@ -4,6 +4,40 @@
     <h1>{{ $barbershop->barbershopName }}</h1>
     <hr>
     <p>About Us: {{ $barbershop->barbershopBio }}</p>
+
+     <form id="needs-validation" method="POST" action="/appt" novalidate>
+      {{ csrf_field() }}
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" name="name" id="name" aria-describedby="nameHelp"
+                   placeholder="Name..." required>
+        </div>
+        <!-- appt time selector -->
+        <div class="form-group">
+            <label for="apptTime">Choose an appointment time:</label>
+            <select multiple class="form-control" name="apptTime" required>
+                <option>09:00</option>
+                <option>10:00</option>
+                <option>11:00</option>
+                <option>12:00</option>
+                <option>13:00</option>
+                <option>14:00</option>
+                <option>15:00</option>
+                <option>16:00</option>
+                <option>17:00</option>
+                <option>18:00</option>
+                <option>19:00</option>
+                <option>20:00</option>
+            </select>
+        </div>
+        <!-- submit -->
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+
+        @include('layouts.errors')
+    </form>
+
     <table class="table table-bordered">
     	<thead>
     		<tr>
@@ -17,5 +51,5 @@
     	@endforeach
     	
     </table>
-    <a href="{{ URL::to('appt/create') }}" class="btn btn-primary btn-lg" role="button">Schedule An Appointment</a>
-@endsection
+<!--     <a href="{{ URL::to('appt/create') }}" class="btn btn-primary btn-lg" role="button">Schedule An Appointment</a>
+ -->@endsection
