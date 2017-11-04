ac3f6f36248d469649102cc1edd7f6e5bf50eaae
Jack Reuter <djreuter45@gmail.com>
2017-11-04 17:33:03 -0600

    cleaned up files by removing some commented out lines of code

--- app/Barbershop.php
+++ app/Barbershop.php
@@ -2,15 +2,11 @@
 
 namespace App;
 
-// use Illuminate\Database\Eloquent\Model;
-
 class Barbershop extends Model {
 
 	public $timestamps = false;
-	
+
     public function appts() {
       return $this->hasMany(Appt::class);
     }
-
-    // protected $fillable = ['barbershopName'];
 }
