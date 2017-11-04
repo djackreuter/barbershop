e9b8f35fd97f5a3c5274691572ba61d4b67d62f2
Jack Reuter <djreuter45@gmail.com>
2017-11-04 17:47:24 -0600

    added route for logout and moved register.create from sessions folder to registration dir

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/register', 'RegistrationController@create');
 Route::post('/register', 'RegistrationController@store');
 
 Route::get('/login', 'SessionsController@create');
+
+Route::get('/logout', 'SessionsController@destroy');
