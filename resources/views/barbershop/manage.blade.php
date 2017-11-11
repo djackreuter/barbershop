ec33d088c8122b0ed8f8a1e3fed5d035a7b2436e
Jack Reuter <djreuter45@gmail.com>
2017-11-10 21:01:46 -0700

    started form to handle delete method

--- resources/views/barbershop/manage.blade.php
+++ resources/views/barbershop/manage.blade.php
@@ -16,7 +16,10 @@
                     <tr>
                         <td class="authTable">{{ $time->apptCustomerName }}</td>
                         <td class="authTable">{{ date('H:i', strtotime($time->apptTime)) }}</td>
-                        <td class="authTable delete-appt"><a href="#">x</a></td>
+                        <td class="authTable delete-appt">{!! Form::open(['method' => 'delete', 'action' => ['ApptController@destroy', $appt->id ]]) !!}
+                            {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+                            {!! Form::close() !!}
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
