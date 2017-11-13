e06989d8f879066fbe6cce2b5c06e982cf71f366
Jack Reuter <djreuter45@gmail.com>
2017-11-13 16:30:52 -0700

    removed Users controller, database migration, and views.  Didn't see a use case for both users and barbershops as users

--- resources/views/users/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-  <ul>
-      <li>User Id: {{ $user->id }}</li>
-      <li>User Name: {{ $user->userName }}</li>
-      <li>User Email: {{ $user->userEmail }}</li>
-  </ul>
-  @endsection
