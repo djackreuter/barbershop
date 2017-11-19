059e527bdb25cb28aa999086c409e0196ab1ec5c
Jack Reuter <djreuter45@gmail.com>
2017-11-19 14:49:38 -0700

    added image to barbershop index page

--- resources/views/barbershop/index.blade.php
+++ resources/views/barbershop/index.blade.php
@@ -3,13 +3,20 @@
 @section('content')
     <h1>Barbershops</h1>
     <hr>
-    <ul>
-        @foreach($barbershops as $barbershop)
-            <li>
-                <a href="/barbershops/{{ $barbershop->id }}">
-                    {{ $barbershop->barbershopName }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="row">
+        <div class="col">
+            <ul>
+            @foreach($barbershops as $barbershop)
+                <li>
+                    <a class="shops-list" href="/barbershops/{{ $barbershop->id }}">
+                        {{ $barbershop->barbershopName }}
+                    </a>
+                </li>
+            @endforeach
+            </ul>
+        </div><!-- /.col -->
+        <div class="col">
+            <img class="image" src="/images/chop.png" alt="scissors">
+        </div><!-- /.col -->    
+    </div><!-- /.row -->
 @endsection
